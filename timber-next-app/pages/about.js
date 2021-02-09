87a6b326ca46ead1a5f96f67869ee63b6ee56b24
Matthew Coburn <mattcoburn24@gmail.com>
2021-02-08 23:11:57 -0800

    add about page info

--- timber-next-app/pages/about.js
+++ timber-next-app/pages/about.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const about = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is the about page</h1>
+    </div>
+  );
 };
 
 export default about;
